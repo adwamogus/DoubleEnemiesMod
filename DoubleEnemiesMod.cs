2bccb5e8bcd5e90aec5741c228ef784e4a6136cc
adwamogus <david.adwa@gmail.com>
2025-10-02 17:13:22 +0200

    it works

--- DoubleEnemiesMod.cs
+++ DoubleEnemiesMod.cs
@@ -7,6 +7,7 @@ using System;
 using System.Collections;
 using System.Linq;
 using System.Reflection;
+using System.Runtime.CompilerServices;
 using UnityEngine;
 using UnityEngine.Device;
 using UnityEngine.SceneManagement;
@@ -45,7 +46,7 @@ public class DoubleEnemiesMod : BaseUnityPlugin
         try
         {
             // Ignore if already a clone
-            if (healthManager.name.Contains("CLONE")) return;
+            if (healthManager.GetComponent<CloneMarker>() != null) return;
 
             // Blacklist check
             foreach (var blocked in Blacklist)
@@ -56,26 +57,30 @@ public class DoubleEnemiesMod : BaseUnityPlugin
                     return;
                 }
             }
-            Log($"HealthManager Enabled: {healthManager.name} in scene {healthManager.gameObject.scene.name}");
-            return;
+
+            // Mark the original object before cloning
+            healthManager.gameObject.AddComponent<CloneMarker>();
 
             // Create clone
             var clone = GameObject.Instantiate(
                 healthManager.gameObject,
-                healthManager.transform.position + Vector3.right * 1f, 
+                healthManager.transform.position + Vector3.right * 0.5f + Vector3.back * 0.01f, 
                 healthManager.transform.rotation,
                 healthManager.transform.parent
             );
 
-            clone.name += "CLONE";
-            healthManager.name += "CLONE";
-
             // Log clone
-            Log($"[Clone] {healthManager.name} -> {clone.name} in scene {healthManager.gameObject.scene.name} @ Pos {healthManager.transform.position}");
+            Log($"[Clone] {healthManager.name} -> {clone.name} in scene {healthManager.gameObject.scene.name}");
         }
         catch (Exception ex)
         {
             Log($"[Error] Error while duplicating {healthManager?.name}: {ex}");
         }
     }
+}
+
+// Empty component for clone detection
+public class CloneMarker : MonoBehaviour
+{
+
 }
